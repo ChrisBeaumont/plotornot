a04283bf6a01a3be4e1e638d7194325cbc633b6a
Chris Beaumont <beaumont@hawaii.edu>
2013-06-28 16:00:58 -0500

    store winners

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file, request, render_template
+from flask import Flask, send_file, request, render_template, redirect
 from matplotlib import rcParamsDefault, rc_context, rcParams, rcParamsOrig
 import matplotlib.pyplot as plt
 from matplotlib.mlab import bivariate_normal
@@ -11,12 +11,14 @@ from glob import glob
 
 app = Flask(__name__)
 
+
 def get_styles():
 
     '''Return 2 random rcParams styles'''
     files = glob('params/*')
     random.shuffle(files)
     return [json.load(open(f)) for f in files[:2]]
+
 
 def mpl_figure_data(f):
     data = StringIO()
@@ -33,7 +35,7 @@ def color_brewer_cycles():
 
 def plot_generator():
     plot_function = random.choice(['plot', 'hist', 'scatter', 'contour'])
-    
+
     N_points = 100
     N_datasets = np.random.randint(2, 8)
     data = []
@@ -45,27 +47,27 @@ def plot_generator():
             d = (x, y)
         elif plot_function == 'hist':
             d = (np.random.normal(np.random.uniform(0, N_datasets*5),
-                                  np.random.uniform(1, 3), 
+                                  np.random.uniform(1, 3),
                                   size=N_points), )
         elif plot_function == 'contour':
-            X, Y = np.meshgrid(np.linspace(0, N_datasets*5, N_points), 
+            X, Y = np.meshgrid(np.linspace(0, N_datasets*5, N_points),
                                np.linspace(0, N_datasets*5, N_points))
-            Z = bivariate_normal(X, Y, 
+            Z = bivariate_normal(X, Y,
                                  np.random.uniform(1, 3),
                                  np.random.uniform(1, 3),
                                  np.random.uniform(0, N_datasets*5),
                                  np.random.uniform(0, N_datasets*5))
             d = (X,Y,Z)
         data.append(d)
-    
+
     kwargs = dict(alpha=np.sqrt(np.random.random()))
     def make_plot(style):
         with rc_context(style):
             rcParams['figure.dpi'] = 75
             rcParams['figure.facecolor'] = '#ffffff'
-            
+
             fig,ax = plt.subplots(1,1,figsize=(6,4))
-            for d in data:    
+            for d in data:
                 getattr(ax, plot_function)(*d, **kwargs)
             ax.set_xlabel('The x axis')
             ax.set_ylabel('The y axis')
@@ -73,16 +75,43 @@ def plot_generator():
             result = mpl_figure_data(fig)
             del fig
         return result
-    
+
     return make_plot
-@app.route("/")
-def main():
+
+
+def serve_page():
     s1, s2 = get_styles()
     assert s1 != s2
-    
+
     plot_func = plot_generator()
     d1, d2 =  map(plot_func, (s1, s2))
-    return render_template('main.html', image_1=d1, image_2=d2)
+
+    return render_template('main.html', image_1=d1, image_2=d2,
+                           style_1=json.dumps(s1), style_2=json.dumps(s2))
+
+
+def save_vote(win, lose):
+    print '%s beats %s' % (win, lose)
+
+
+@app.route('/vote/<int:winner>', methods=['POST'])
+def vote(winner):
+    data = request.values
+    left = data['left']
+    right = data['right']
+
+    if winner == 0:
+        win, lose = left, right
+    else:
+        win, lose = right, left
+    save_vote(win, lose)
+
+    return redirect('/')
+
+
+@app.route("/")
+def main():
+    return serve_page()
 
 
 if __name__ == "__main__":
