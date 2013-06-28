5e35928d79a2d4e20fc63d3e69f3c5f71ad4b559
Chris Beaumont <beaumont@hawaii.edu>
2013-06-28 16:19:57 -0500

    tweak app running for heroku

--- app.py
+++ app.py
@@ -153,6 +153,7 @@ def vote(winner):
 def main():
     return serve_page()
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    debug = '--debug' in sys.argv
+    app.run(host='0.0.0.0', port=port, debug=debug)
